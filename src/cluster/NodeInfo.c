d5729feb0cb15f05066b41db75cfbd006fbd1f50
ray <rjenkins@Rays-Macbook-Pro.local>
2013-08-18 19:44:39 -0500

    strndup and strnlen

--- src/cluster/NodeInfo.c
+++ src/cluster/NodeInfo.c
@@ -14,16 +14,16 @@ NodeInfo* get_node_info(jsmntok_t tokens[], char *buffer) {
   end = tokens[4].end;
   char *connectionID = substring(start, end, buffer);
 
-  struct NodeInfo *node_info = malloc(sizeof(struct NodeInfo));
-  node_info->state = strdup(state);
-  node_info->connection_id = strdup(connectionID);
+  struct NodeInfo *node_info = calloc(1, sizeof(struct NodeInfo));
+  node_info->state = strndup(state, strnlen(state, 1024));
+  node_info->connection_id = strndup(connectionID, strnlen(connectionID, 1024));
 
   return node_info;
 }
 
 char *substring(int start, int end, char* buffer) {
 
-  char *string = malloc((end - start) + 1);
+  char *string = calloc(1, (end - start) + 1);
   char *s = string;
   char *p = buffer;
   p = p + start;
